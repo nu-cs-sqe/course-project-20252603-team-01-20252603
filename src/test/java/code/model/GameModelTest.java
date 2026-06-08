2094c1970fb6cf8dc4866522e7973e5aaf5f260a
Aanand Patel <aanandp2003ca@gmail.com>
2026-06-08 14:27:32 -0500

    game model continent detail tests now pass

--- src/test/java/code/model/GameModelTest.java
+++ src/test/java/code/model/GameModelTest.java
@@ -12,6 +12,18 @@ public final class GameModelTest {
 
     private static final int CONTINENT_COUNT = 6;
     private static final int TERRITORY_COUNT = 42;
+    private static final int NORTH_AMERICA_TERRITORY_COUNT = 9;
+    private static final int SOUTH_AMERICA_TERRITORY_COUNT = 4;
+    private static final int EUROPE_TERRITORY_COUNT = 7;
+    private static final int AFRICA_TERRITORY_COUNT = 6;
+    private static final int ASIA_TERRITORY_COUNT = 12;
+    private static final int AUSTRALIA_TERRITORY_COUNT = 4;
+    private static final int NORTH_AMERICA_BONUS = 5;
+    private static final int SOUTH_AMERICA_BONUS = 2;
+    private static final int EUROPE_BONUS = 5;
+    private static final int AFRICA_BONUS = 3;
+    private static final int ASIA_BONUS = 7;
+    private static final int AUSTRALIA_BONUS = 2;
 
     @Test
     public void gameModelConstructsWithEmptyContinents() {
@@ -69,5 +81,67 @@ public final class GameModelTest {
                 .allMatch(territory -> territory.getArmyCount() == 0);
 
         assertTrue(allHaveZeroArmies);
+    }
+
+    private Continent findContinent(
+            final GameModel gameModel,
+            final String continentName) {
+        return gameModel.getContinents()
+                .stream()
+                .filter(continent -> continent.getName().equals(continentName))
+                .findFirst()
+                .get();
+    }
+
+    @Test
+    public void eachContinentHasCorrectTerritoryCount() {
+        GameModel gameModel = new GameModel();
+
+        gameModel.initializeContinentsAndTerritories();
+
+        assertEquals(
+                NORTH_AMERICA_TERRITORY_COUNT,
+                findContinent(gameModel, "North America").getTerritories().size());
+        assertEquals(
+                SOUTH_AMERICA_TERRITORY_COUNT,
+                findContinent(gameModel, "South America").getTerritories().size());
+        assertEquals(
+                EUROPE_TERRITORY_COUNT,
+                findContinent(gameModel, "Europe").getTerritories().size());
+        assertEquals(
+                AFRICA_TERRITORY_COUNT,
+                findContinent(gameModel, "Africa").getTerritories().size());
+        assertEquals(
+                ASIA_TERRITORY_COUNT,
+                findContinent(gameModel, "Asia").getTerritories().size());
+        assertEquals(
+                AUSTRALIA_TERRITORY_COUNT,
+                findContinent(gameModel, "Australia").getTerritories().size());
+    }
+
+    @Test
+    public void eachContinentHasCorrectBonusArmyValue() {
+        GameModel gameModel = new GameModel();
+
+        gameModel.initializeContinentsAndTerritories();
+
+        assertEquals(
+                NORTH_AMERICA_BONUS,
+                findContinent(gameModel, "North America").getBonusArmies());
+        assertEquals(
+                SOUTH_AMERICA_BONUS,
+                findContinent(gameModel, "South America").getBonusArmies());
+        assertEquals(
+                EUROPE_BONUS,
+                findContinent(gameModel, "Europe").getBonusArmies());
+        assertEquals(
+                AFRICA_BONUS,
+                findContinent(gameModel, "Africa").getBonusArmies());
+        assertEquals(
+                ASIA_BONUS,
+                findContinent(gameModel, "Asia").getBonusArmies());
+        assertEquals(
+                AUSTRALIA_BONUS,
+                findContinent(gameModel, "Australia").getBonusArmies());
     }
 }
