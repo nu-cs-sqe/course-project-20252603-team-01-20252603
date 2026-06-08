b10cb0972118ec0d7e1f9865ecb43615f1316df3
Aanand Patel <aanandp2003ca@gmail.com>
2026-06-08 13:27:44 -0500

    feat: reject continent bonus armies above maximum

--- src/main/java/Code/model/Continent.java
+++ src/main/java/Code/model/Continent.java
@@ -6,12 +6,13 @@ package code.model;
 public final class Continent {
 
     private static final int MIN_BONUS_ARMIES = 2;
+    private static final int MAX_BONUS_ARMIES = 7;
 
     private final String name;
     private final int bonusArmies;
 
     public Continent(final String continentName, final int continentBonusArmies) {
-        validateMinimumBonusArmies(continentBonusArmies);
+        validateBonusArmies(continentBonusArmies);
 
         name = continentName;
         bonusArmies = continentBonusArmies;
@@ -25,9 +26,11 @@ public final class Continent {
         return bonusArmies;
     }
 
-    private void validateMinimumBonusArmies(final int continentBonusArmies) {
-        if (continentBonusArmies < MIN_BONUS_ARMIES) {
-            throw new IllegalArgumentException("Bonus armies are below minimum.");
+    private void validateBonusArmies(final int continentBonusArmies) {
+        if (continentBonusArmies < MIN_BONUS_ARMIES
+                || continentBonusArmies > MAX_BONUS_ARMIES) {
+            throw new IllegalArgumentException("Invalid continent bonus armies.");
         }
     }
+
 }
