c2dc162c0fb6bc662eb6c58a7050c6b37c43b5ad
Aanand Patel <aanandp2003ca@gmail.com>
2026-06-08 15:44:31 -0500

    game controller start game test now passes

--- src/test/java/code/controller/GameControllerTest.java
+++ src/test/java/code/controller/GameControllerTest.java
@@ -12,6 +12,12 @@ import org.junit.jupiter.api.Test;
  */
 public final class GameControllerTest {
 
+    private static final int CONTINENT_COUNT = 6;
+
+    private static final int TERRITORY_COUNT = 42;
+
+    private static final int DECK_CARD_COUNT = 44;
+
     @Test
     public void gameControllerConstructsWithDefaultDependencies() {
         new GameController();
