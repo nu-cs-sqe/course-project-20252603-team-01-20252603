10ee812acc0b78af77825d12f3cac5bf633dba32
Aanand Patel <aanandp2003ca@gmail.com>
2026-06-09 17:52:14 -0500

    game model returns middle current player name

--- src/test/java/code/model/GameModelTest.java
+++ src/test/java/code/model/GameModelTest.java
@@ -591,5 +591,18 @@ public final class GameModelTest {
         assertEquals("Player 1", gameModel.getCurrentPlayerName());
     }
 
+    @Test
+    public void getCurrentPlayerNameMiddlePlayerIndexReturnsMiddlePlayerName() {
+        GameModel gameModel = new GameModel();
+
+        gameModel.setPlayerCount(MIN_PLAYER_COUNT);
+        gameModel.addPlayer("Player 1", PlayerColor.RED);
+        gameModel.addPlayer("Player 2", PlayerColor.BLUE);
+        gameModel.addPlayer("Player 3", PlayerColor.GREEN);
+        gameModel.setCurrentPlayerIndex(1);
+
+        assertEquals("Player 2", gameModel.getCurrentPlayerName());
+    }
+
 
 }
